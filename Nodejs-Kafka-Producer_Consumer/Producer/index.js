a4f6e63db0a04373fe885bba110ce17be7bfb549
Denizhan <65414809+Elmarath@users.noreply.github.com>
2022-06-03 00:18:45 +0300

    First Working Demo added

---
+++ Nodejs-Kafka-Producer_Consumer/Producer/index.js
@@ -0,0 +1,22 @@
+console.log("producer...");
+
+import Kafka from 'node-rdkafka'
+
+const stream = Kafka.Producer.createWriteStream({
+  'metadata.broker.list': 'localhost:9092'
+}, {}, {topic: 'test'});
+
+function queueMessage(){
+  const success = stream.write(Buffer.from('Hi '));
+  if(success)
+  {
+    console.log("Message wrote successfully to stream");
+  }
+  else {
+    console.log("An Error occured while writing message to stream");
+  }
+}
+
+setInterval(() => {
+  queueMessage();
+}, 3000);
